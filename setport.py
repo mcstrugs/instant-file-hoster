33d07dc129d2aed41428e141bc489f93a3eee17e
joemartinson <martinsonjoe2@gmail.com>
2020-08-20 16:50:55 -0700

    implemented set port

--- setport.py
+++ setport.py
@@ -5,6 +5,16 @@ import sys
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 0))
-addr = s.getsockname()
-print(addr[1])
+addr = s.getsockname()[1]
 s.close()
+
+r = open("./output/docker-compose.yml","r")
+data = r.read()
+r.close()
+
+#print(data)
+data = data.replace('XXXX',str(addr))
+#print(data)
+w = open("./output/docker-compose.yml","wt")
+w.write(data)
+w.close()
